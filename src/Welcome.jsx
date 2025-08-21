809556cc76774c1055ef88b94227d3d5497b6f59
Dinesh Kumar <dinesh78088@gmail.com>
2025-08-21 21:19:29 +0530

    project created

---
+++ src/Welcome.jsx
@@ -0,0 +1,11 @@
+function Welcome() {
+   
+    return(
+        <>
+            <h1>Welcome to Dev Brain Box by Digital Vision Academy</h1>       
+        </>
+    )
+   
+}
+
+export default Welcome
